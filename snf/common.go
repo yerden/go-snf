10eead2deb8e246143f99b4386021819bd624a86
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-02-05 12:02:04 +0600

    add error helper functions
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- snf/common.go
+++ snf/common.go
@@ -24,3 +24,23 @@ func array2Slice(ptr uintptr, length int) (data []byte) {
 	sh.Cap = length
 	return
 }
+
+// Return true if an error is EAGAIN.
+func IsEagain(err error) bool {
+	return syscall.Errno(syscall.EAGAIN) == err
+}
+
+// Return true if an error is EINVAL.
+func IsEinval(err error) bool {
+	return syscall.Errno(syscall.EINVAL) == err
+}
+
+// Return true if an error is ENODEV.
+func IsEnodev(err error) bool {
+	return syscall.Errno(syscall.ENODEV) == err
+}
+
+// Return true if an error is EBUSY.
+func IsEbusy(err error) bool {
+	return syscall.Errno(syscall.EBUSY) == err
+}
