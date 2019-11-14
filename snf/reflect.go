37482a7c41b02f41805a7c6996584e783d39426e
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-11-14 17:31:16 +0600

    snf: rework reflect
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- snf/reflect.go
+++ snf/reflect.go
@@ -9,8 +9,6 @@ package snf
 import "C"
 
 import (
-	"io"
-	"sync/atomic"
 	"unsafe"
 )
 
@@ -24,21 +22,16 @@ import (
 // back in the kernel. The implementation makes no explicit step to make the
 // kernel-based processing any faster than it is when Sniffer is not being used
 // (in fact, it is probably much slower).
-type ReflectHandle struct {
-	state *int32
-	dev   C.snf_netdev_reflect_t
-}
+type ReflectHandle C.char
 
 // ReflectEnable enables a network device for packet reflection and returns
 // ReflectHandle.
 //
 // As stated in SNF documentation, this call is always a success.
 func (h *Handle) ReflectEnable() (*ReflectHandle, error) {
-	ref := &ReflectHandle{
-		state: &h.state,
-		dev:   nil,
-	}
-	return ref, retErr(C.snf_netdev_reflect_enable(h.dev, &ref.dev))
+	var ref *ReflectHandle
+	return ref, retErr(C.snf_netdev_reflect_enable(handle(h),
+		(*C.snf_netdev_reflect_t)(unsafe.Pointer(&ref))))
 }
 
 // Reflect a packet to the network device.
@@ -51,8 +44,6 @@ func (h *Handle) ReflectEnable() (*ReflectHandle, error) {
 // package's Reflect will return io.EOF error in case the underlying Handle is
 // about to close due to signal or user Close call.
 func (ref *ReflectHandle) Reflect(pkt []byte) error {
-	if atomic.LoadInt32(ref.state) != stateOk {
-		return io.EOF
-	}
-	return retErr(C.snf_netdev_reflect(ref.dev, unsafe.Pointer(&pkt[0]), C.uint(len(pkt))))
+	return retErr(C.snf_netdev_reflect(C.snf_netdev_reflect_t(ref),
+		unsafe.Pointer(&pkt[0]), C.uint(len(pkt))))
 }
