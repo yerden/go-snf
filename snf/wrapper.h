cbdfe860cdd55bbfc2289d38641c89d4a2f55806
Nadir Nurlybekov <n.nurlybekov@kazdream.kz>
2022-08-11 17:05:47 +0600

    snf: remove mock for snf_inject_send_bulk
    Signed-off-by: Nadir Nurlybekov <n.nurlybekov@kazdream.kz>

--- snf/wrapper.h
+++ snf/wrapper.h
@@ -261,13 +261,6 @@ static int snf_inject_send(snf_inject_t inj, int timeout_ms, int flags,
 	return ENOTSUP;
 }
 
-static inline struct compound_int snf_inject_send_bulk(snf_inject_t inj,
-				int timeout_ms, int flags, uintptr_t *pkts, uint32_t n_pkts,
-				const uint32_t *lengths)
-{
-	return ENOTSUP;
-}
-
 static int snf_inject_sched(snf_inject_t inj, int timeout_ms, int flags,
 			    const void *pkt, uint32_t length, uint64_t delay_ns)
 {
